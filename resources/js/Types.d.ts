b5f7a33fcdbad25064fd6950c5fd60b9e40e2725
Narsilien <rigaux.jonathan@gmail.com>
2024-08-03 06:45:39 +0200

    Add model types

---
+++ resources/js/Types.d.ts
@@ -0,0 +1,15 @@
+type IconModel = {
+	active: boolean;
+	created_at: string;
+	id: number;
+	path: string;
+	updated_at: string;
+};
+
+type ImageModel = {
+	active: boolean;
+	created_at: string;
+	id: number;
+	path: string;
+	updated_at: string;
+};
